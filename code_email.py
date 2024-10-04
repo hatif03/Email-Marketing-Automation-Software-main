1555f966ac7b801b53d269730720ba8818b1f47b
Mohammad Hatif Osmani <66199079+hatif03@users.noreply.github.com>
2024-10-05 00:06:06 +0530

    added utf encoding + gitignore

--- code_email.py
+++ code_email.py
@@ -6,7 +6,7 @@ def Email_send_function(to, subject, message, uname, pasw):
     s = smtplib.SMTP("smtp.gmail.com", 587)  # create session for gmail
     s.starttls()  # transport layer
     s.login(uname, pasw)
-    msg = "Subject: {}\n\n{}".format(subject, message)
+    msg = "Subject: {}\n\n{}".format(subject, message).encode('utf-8')
     s.sendmail(uname, to, msg)
     x = s.ehlo()
     if x[0] == 250:
